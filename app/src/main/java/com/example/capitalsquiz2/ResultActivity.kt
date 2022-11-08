4d3364db7d2b929678ebf7aed723e7abe4130c94
jamuratov <jamuratovnietbay10@gmail.com>
2022-11-08 17:27:27 +0500

    Quiz app bolindi

--- app/src/main/java/com/example/capitalsquiz2/ResultActivity.kt
+++ app/src/main/java/com/example/capitalsquiz2/ResultActivity.kt
@@ -1,11 +1,34 @@
 package com.example.capitalsquiz2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import com.example.capitalsquiz2.databinding.ActivityGameBinding
+import com.example.capitalsquiz2.databinding.ActivityResultBinding
 
 class ResultActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityResultBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_result)
+        binding = ActivityResultBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val score = intent.getStringExtra(Constants.score)
+        val totalQuestion = intent.getStringExtra("total size")
+
+        binding.apply {
+            "$score / $totalQuestion \n".also { RightAnswer.text = it }
+
+        }
+
+        binding.newgame.setOnClickListener {
+            val intent = Intent(this , GameActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+        binding.exit.setOnClickListener {
+            val intent = Intent(this,ResultActivity::class.java)
+            finish()
+        }
     }
 }
